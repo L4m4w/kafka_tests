bf3bdf89aee041d544c5b9afa716bc88ed2a1d6e
Evgen Larkin <l4p41@yandex.com>
2026-06-28 23:13:20 +0300

    Kafka producer interface added; Kafka test added;

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
+import uuid
+
 import pytest
 
 from frame.internal.http.account import AccountApi
 from frame.internal.http.mail import MailApi
+from frame.internal.kafka.kafka_base import KafkaProducerApi
+
 
 @pytest.fixture(scope="session")
 def account() -> AccountApi:
@@ -10,3 +14,19 @@ def account() -> AccountApi:
 @pytest.fixture(scope="session")
 def mail() -> MailApi:
     return MailApi()
+
+@pytest.fixture(scope="session")
+def kafka_producer() -> KafkaProducerApi:
+    with KafkaProducerApi() as producer:
+        yield producer
+
+@pytest.fixture()
+def registration_message() -> dict:
+    base = uuid.uuid4().hex
+    expected_mail = f"{base}ng@mail.ru"
+    message = {
+        "login": f"{base}",
+        "email": expected_mail,
+        "password":"string"
+    }
+    return message
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,9 +2,11 @@ import time
 import uuid
 
 import pytest
+from tomlkit import value
 
 from frame.internal.http.account import AccountApi
 from frame.internal.http.mail import MailApi
+from frame.internal.kafka.kafka_base import KafkaProducerApi
 
 
 def test_failed_registration(account: AccountApi, mail: MailApi):
@@ -19,13 +21,25 @@ def test_failed_registration(account: AccountApi, mail: MailApi):
 
 
 
-def test_success_registration(account: AccountApi, mail: MailApi):
-    base = uuid.uuid4().hex
-    expectrd_mail = f"{base}ng@mail.ru"
-    account.register_user(login=f"{base}", email=expectrd_mail, password="string")
+def test_success_registration(account: AccountApi, mail: MailApi, registration_message: dict):
+    account.register_user(
+        **registration_message
+    )
 
     for _ in range(10):
-        responce = mail.search_mail(query=expectrd_mail)
+        responce = mail.search_mail(query=registration_message.get("email"))
+        if responce.json()["total"] > 0:
+            break
+        time.sleep(1)
+    else:
+        raise AssertionError("Email not found")
+
+def test_success_registration_with_kafka(kafka_producer: KafkaProducerApi, registration_message: dict, mail: MailApi):
+
+    kafka_producer.send(topic="register-events", value=registration_message)
+
+    for _ in range(10):
+        responce = mail.search_mail(query=registration_message.get("email"))
         if responce.json()["total"] > 0:
             break
         time.sleep(1)
